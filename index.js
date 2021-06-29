3dd1f1459ad24b9313950fc528fb4e4fdf52625c
Dave <sheepysheep60@hotmail.com>
2021-06-29 10:22:48 +0100

    updated module

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*
   ~ The class
 */
-exports.ShapedRandom = {
+module.exports = {
   // Bias should be a float between 0 and 1, 1 being 100% of results are 1
   bias(bias = 0.8){
     let x = Math.random();
